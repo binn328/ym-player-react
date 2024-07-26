b64fb19566102e586756b1a451cb87521a8bb6bb
binn328 <binn328@gmail.com>
2024-07-26 10:50:51 +0900

    edit route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
                 <Route path="/player" element={<div>플레이어</div>} />
                 <Route path="/album" element={<div>앨범</div>} />
                 <Route path="/playlist" element={<div>재생목록</div>} />
-                <Route path="/downlaod" element={<div>다운로드</div>} />
+                <Route path="/download" element={<div>다운로드</div>} />
             </Routes>
         </>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,34 @@
 import Icon from "@mdi/react";
 import { mdiMenu, mdiDotsHorizontal } from "@mdi/js";
 import ThemeControllButton from "./ThemeControllButton";
+import { useNavigate } from "react-router-dom";
+
 function Navbar() {
+    let nevigate = useNavigate();
+
     return (
         <div className="navbar bg-base-100">
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
+                {/* <button className="btn btn-square btn-ghost">
                     <Icon path={mdiMenu} size={1} />
-                </button>
+                </button> */}
             </div>
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">YM Player</a>
+                <a className="btn btn-ghost text-xl" onClick={() => nevigate("/")}>
+                    YM Player
+                </a>
                 <ThemeControllButton />
             </div>
             <div>
-                <ul className="menu menu-horizontal bg-base-200">
+                <ul className="menu menu-horizontal ">
                     <li>
-                        <a>Item 1</a>
+                        <a onClick={() => nevigate("/album")}>Album</a>
                     </li>
                     <li>
-                        <a>Item 2</a>
+                        <a onClick={() => nevigate("/playlist")}>Playlist</a>
                     </li>
                     <li>
-                        <a>Item 3</a>
+                        <a onClick={() => nevigate("/download")}>Download</a>
                     </li>
                 </ul>
             </div>
